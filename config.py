a67a24af32e468081ec6290ac1080218a8436cd9
Kevin Samson <kevinsam2003@gmail.com>
2021-03-19 12:53:58 +0400

    Removed info

--- config.py
+++ config.py
@@ -15,7 +15,7 @@ class Config:
     FLASK_DEBUG = os.getenv('FLASK_DEBUG')
     SECRET_KEY = os.getenv('SECRET_KEY')
     SERVER = os.getenv('SERVER')
-    CLIENT_ID = '44de3ff0881d48c8bf95d687e5c54980'
-    CLIENT_SECRET = 'cb44c266456f483bb9efa4b092de9192'
-    REDIRECT_URI = 'http://localhost:5000/api_callback'
-    SCOPE = 'user-library-read user-read-currently-playing user-read-playback-state'
+    CLIENT_ID = os.getenv('CLIENT_ID')
+    CLIENT_SECRET = os.getenv('CLIENT_SECRET')
+    REDIRECT_URI = os.getenv('REDIRECT_URI')
+    SCOPE = os.getenv('SCOPE')
